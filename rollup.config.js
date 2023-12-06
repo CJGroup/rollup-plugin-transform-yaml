5cfdd578f37b8150ecde8b470553299b3ff25f67
ConstasJ <constasj@qq.com>
2023-12-06 16:01:36 +0800

    chore: 将模块改为cjs和esm共存

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,34 @@
 import ts from 'rollup-plugin-ts';
 import { defineConfig } from 'rollup';
 
-export default defineConfig({
-    input: 'src/index.ts',
-    output: {
-        format: 'esm',
-        file: 'lib/index.js',
+export default defineConfig([
+    {
+        input: 'src/index.ts',
+        output: {
+            format: 'esm',
+            file: 'lib/index.mjs',
+        },
+        external: ['fs/promises', 'path', 'js-yaml'],
+        plugins: [
+            ts({
+                tsconfig: {
+                    fileName: 'tsconfig.json',
+                    hook: (resolvedConfig) => ({...resolvedConfig, declaration: false})
+                }
+            })
+        ]
     },
-    plugins: [
-        ts({
-            tsconfig: 'tsconfig.json',
-        })
-    ],
-})
+    {
+        input: 'src/index.ts',
+        output: {
+            format: 'cjs',
+            file: 'lib/index.js',
+        },
+        external: ['fs/promises', 'path', 'js-yaml'],
+        plugins: [
+            ts({
+                tsconfig: 'tsconfig.json',
+            })
+        ]
+    }
+])
